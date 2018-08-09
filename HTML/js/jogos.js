b0854b49d0059c653fa61d910d05c8a3b3ef4f60
PRElias <ppuspfc@gmail.com>
2018-08-09 18:04:38 -0300

    Página de jogos

--- HTML/js/jogos.js
+++ HTML/js/jogos.js
@@ -1,25 +1,58 @@
-var urlpronta = "http://paulorobertoelias.com.br/api/api/jogos/getsteamall";
+var app = {
+    isLoading: true,
+    visibleCards: {},
+    games: [],
+    spinner: document.querySelector('#div_loading')
+};
 
-$.get(urlpronta, function (retorno) {
+app.getGames = function (force) {
+    var url = 'http://mycollectionsapi.paulorobertoelias.com.br/api/Games';
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                app.games = request.response;
+                localStorage.setItem("games", app.games);
+            }
+        }
+    };
+    request.open('GET', url, false);
+    request.send();
 
-    $("#div_loading").css("display", "none");
+    app.renderizeGames();
+    app.spinner.setAttribute('hidden', true);
 
+};
+
+app.renderizeGames = function () {
     var items = [];
+    var games = JSON.parse(app.games);
+    for (var index in games) {
+        var game = games[index];
 
-    $.each(retorno.Jogos, function (key) {
         items.push(
-            (retorno.Jogos[key].ImageIconURL.substr((retorno.Jogos[key].ImageIconURL.length - 5)) != "/.jpg" ? "<a href='#' data-toggle='tooltip' title='" + retorno.Jogos[key].Name + " ***Tempo jogado: " + retorno.Jogos[key].Playtime +  "***'>" +
-            "<img src='" + retorno.Jogos[key].ImageIconURL + "'.jpg' class='estante'></a>" : "")
+            "<div class='row' id='" + game.gameID + "'>" +
+            //"<p>" + game.name + "</p > " +
+            "<img class='col cover' src='" + game.cover + "' alt='logo' data-toggle='modal' data-target='#myModal" + game.gameID + "' /img>" +
+            "</div><div id='myModal" + game.gameID + "' class='modal fade' role='dialog'>" +
+            "<div class='modal-dialog mymodal'>" +
+            "<div class='modal-content mymodal-content'>" +
+            "<div class='modal-header'>" +
+            "<h6 class='modal-title'>" + game.name + "</h6>" +
+            "</div><div class='modal-body'>" +
+            "<p><small>Informações sobre o jogo</small></p>" +
+            //"<button type='button' class='close' data-dismiss='modal'>Fechar</button>" +
+            "</div></div></div></div>"
         );
-    });
+    }
 
+    var wrapper = document.createElement('div');
+    wrapper.innerHTML = items.join("");
 
-    $("#resultado").empty();
-
-    $("<div/>", {
-        "class": "",
-        html: items.join("")
-    }).appendTo("#resultado");
-
+    var main = document.querySelector('div.main_div');
+    main.appendChild(wrapper);
     
-});
+};
+window.onload = function(){
+    app.getGames(false);
+}
