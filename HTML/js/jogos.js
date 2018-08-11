d5b5ed5e83fa81ffc0733b58122d4dc2262d5fb3
Paulo Roberto Elias <ppuspfc@gmail.com>
2018-08-11 17:25:53 -0300

    modal jogos com detalhes

--- HTML/js/jogos.js
+++ HTML/js/jogos.js
@@ -2,6 +2,7 @@ var app = {
     isLoading: true,
     visibleCards: {},
     games: [],
+    gameDetails: [],
     spinner: document.querySelector('.loader')
 };
 
@@ -22,6 +23,22 @@ app.getGames = function (force) {
     app.renderizeGames();
     app.spinner.setAttribute('hidden', true);
 
+};
+
+app.getGameDetails = function (gameName) {
+    var url = 'http://mycollectionsapi.paulorobertoelias.com.br/api/Games/' + gameName;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                app.gameDetails = request.response;
+            }
+        }
+    };
+    request.open('GET', url, false);
+    request.send();
+
+    app.renderizeDetails();
 };
 
 app.renderizeGames = function () {
@@ -46,6 +63,28 @@ app.renderizeGames = function () {
 
 };
 
+app.renderizeDetails = function () {
+    var items = [];
+    var game = JSON.parse(app.gameDetails);
+    for (var index in game) {
+        var copy = game[index];
+
+        items.push(
+            "<img src='" + copy.storeLogo + "' class='storeLogo' /img>" 
+        );
+    }
+
+    var wrapper = document.createElement("div");
+    wrapper.innerHTML = items.join("");
+
+    var modal = document.querySelector('div#myModalBody');
+    modal.innerHTML = '';
+    modal.appendChild(wrapper);
+
+};
+
+
+
 window.onload = function () {
     app.getGames(false);
     $('#myModal').on('show.bs.modal', function (event) {
@@ -53,5 +92,6 @@ window.onload = function () {
         var button = $(event.relatedTarget)
         var recipient = button.data('game')
         modal.find('.modal-title').text(recipient)
+        app.getGameDetails(recipient);
     });
 }
